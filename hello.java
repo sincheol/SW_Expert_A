a3af5f6d55126d3b9fa9f0c31305ea585aaf21c1
sincheol <ohmong0803@gmail.com>
2022-12-25 23:54:20 +0900

    2try

--- hello.java
+++ hello.java
@@ -5,7 +5,7 @@ public class hello {
         int testCase = 1; // Number of test case
         int inputNumber = 0; // Number of input
         int sum = 0;
-        int maxSum = 0; // Store Max sum
+        int maxSum = -1001; // Store Max sum
 
         // store tc
         Scanner scanner = new Scanner(System.in);
@@ -19,7 +19,7 @@ public class hello {
             String sInput = scanner.nextLine();
             String sarrInput[] = sInput.split(" ");
 
-            //error
+            // error
             if (sarrInput.length != inputNumber) {
                 System.out.println("invalid input");
                 System.exit(0);
@@ -28,15 +28,16 @@ public class hello {
             int iarrInput[] = new int[sarrInput.length];
 
             // find Max sum
-            for(int n=0;n<sarrInput.length;n++){
-                iarrInput[n] = Integer.parseInt(sarrInput[n]); //convert string to int
-                sum = sum + iarrInput[n];
-
-                //if sum value is negative reset
+            for (int n = 0; n < sarrInput.length; n++) {
+                //if sum value is negative reset 
                 if(sum<0){
                     sum=0;
-                }
-                //update max sum
+                } 
+                    
+                iarrInput[n] = Integer.parseInt(sarrInput[n]); //convert string to int
+                sum = sum + iarrInput[n];
+ 
+                // update  max sum
                 if(maxSum<sum){
                     maxSum = sum;
                 }
