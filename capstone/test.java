c9f1742690e6fe55270ffb2c66bad56857f1bead
sincheol <ohmong0803@gmail.com>
2023-10-20 14:46:25 +0900

    del, add id

--- capstone/test.java
+++ capstone/test.java
@@ -1,26 +1,31 @@
 package capstone;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class test {
-    int t = 1;
-
-    public void test1() {
-        if (t <= 3) {
-            t++;
-            test2(t);
-            System.out.println(t);
-        }
-    }
-
-    public void test2(int t) {
-        if (t <= 3) {
-            test1();
-            System.out.println(t);
-        }
-    }
-
-    // node root, priorityqueue<node>
     public static void main(String[] args) {
-        test a = new test();
-        a.test1();
+        int numCoordinates = 600000; // 원하는 좌표 개수
+        Set<String> coordinatesSet = new HashSet<>();
+        Random random = new Random();
+
+        while (coordinatesSet.size() < numCoordinates) {
+            int x = random.nextInt(100000) + 1; // 1부터 100000까지의 랜덤 x 좌표
+            int y = random.nextInt(100000) + 1; // 1부터 100000까지의 랜덤 y 좌표
+            coordinatesSet.add(x + "," + y);
+        }
+
+        try {
+            FileWriter writer = new FileWriter("random.txt");
+            for (String coordinates : coordinatesSet) {
+                writer.write(coordinates + "\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
